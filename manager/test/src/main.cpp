5fe51a38c742671d04d9dba9fbc9f6b311d828a9
Di Wu <allwu@falcon-computing.com>
2019-01-04 06:35:56 -0800

    fix mysterious seg fault in unit test; update altr_opencl:OpenCLQueueManager on kenrel creation error

--- manager/test/src/main.cpp
+++ manager/test/src/main.cpp
@@ -7,6 +7,6 @@ int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   google::InitGoogleLogging(argv[0]);
   FLAGS_alsologtostderr = true;
-  FLAGS_v = 1;
+  FLAGS_v = 2;
   return RUN_ALL_TESTS();
 }
